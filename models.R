2ae5dab1cb08eed62ea02f21ce14e50c65cd5042
Zachary M. Jones <zmj@zmjones.com>
2013-07-27 10:52:15 -0400

    formatting changes

--- models.R
+++ models.R
@@ -10,15 +10,15 @@ df$polpris <- factor(df$polpris)
 df$tort <- factor(df$tort)
 df$gdppc <- log(df$gdppc)
 df$pop <- log(df$pop)
-df.save <- df
+df.save <- df[!(is.na(df$amnesty) | is.na(df$physint)), ]
 df <- na.omit(df)
 
 lrm.vars <- c("disap", "kill", "polpris", "tort", "amnesty")
 ols.vars <- c("physint", "amnesty")
 ciri.vars <- lrm.vars[1:4]
 base.spec <- "~ log(gdppc) + log(pop)"
-ivars <- colnames(df)[!colnames(df) %in% c("ccode", "year", ciri.vars, "physint", "amnesty",
-                                           "gdppc", "pop")]
+ivars <- colnames(df)[!colnames(df) %in% c("ccode", "year", ciri.vars,
+                                           "physint", "amnesty", "gdppc", "pop")]
 specs <- paste0("~ gdppc + pop + ", ivars)
 specs.cwar <- paste0("~ gdppc + pop + cwar + ", ivars[!(ivars %in% "cwar")])
 ivar.labels <- c("INGOs", "Polity", "Executive Compet.", "Executive Open.",
@@ -75,10 +75,9 @@ PlotCV(cv[[12]], "cv-pts-ols", "Political Terror Scale (OLS)", "RMSE")
 PlotCV(cv[[13]], "cv-cwar-physint", "Physical Integrity Index (OLS)", "RMSE")
 PlotCV(cv[[14]], "cv-cwar-pts-ols", "Physical Integrity Index (OLS)", "RMSE")
 
-df <- df.save[!(is.na(df.save$amnesty) | is.na(df.save$physint)), ]
 imp <- mclapply(c(lrm.vars, "physint"), function(x)
                 varimp(cforest(as.formula(paste0(x, "~", paste0(ivars, collapse = "+"))),
-                data = df)), mc.cores = CORES)
+                data = df.save)), mc.cores = CORES)
 PlotImp(imp[[1]], "Disappearances, Variable Importance", "disap-imp", ivar.labels)
 PlotImp(imp[[2]], "Killings, Variable Importance", "kill-imp", ivar.labels)
 PlotImp(imp[[3]], "Political Imprisonment, Variable Importance", "polpris-imp", ivar.labels)
