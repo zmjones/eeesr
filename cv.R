1bf167d46ea01a8fd5eae83b02dec16ce5205317
Zachary M. Jones <zmj@zmjones.com>
2013-08-17 09:46:38 -0400

    cross validation now supports mi
    each model is cross validated on all of the imputed data sets. after the summary statistics of the prediction error for each cross-validated model is calculated, they are averaged across all imputed data sets. this should really occur *before* the summary statistics are calculated though.

--- cv.R
+++ cv.R
@@ -24,9 +24,9 @@ CleanCV <- function(cv) {
   return(cv)
 }
 
-CallCV <- function(specs, depvar, mtype, rnames) {
+CallCV <- function(data, specs, depvar, mtype, rnames) {
   cv <- CleanCV(t(as.data.frame(mclapply(specs, function(x)
-                CVrms(depvar, model.matrix(as.formula(x), df)[, -1],
+                CVrms(depvar, model.matrix(as.formula(x), data)[, -1],
                 CV_FOLD, CV_ITER, model = mtype), mc.cores = CORES))))
   cv$spec <- rnames
   return(cv)
@@ -34,12 +34,22 @@ CallCV <- function(specs, depvar, mtype, rnames) {
 
 specs <- c("~ gdppc + pop", specs)
 specs.cwar <- c("~ gdppc + pop + cwar", specs.cwar)
-cv.lrm <- lapply(lrm.vars, function(x) CallCV(specs, df[, x], "lrm",
-                 c("log GDP per cap. + log Pop.", ivar.labels)))
-cv.lrm.cwar <- lapply(lrm.vars, function(x) CallCV(specs.cwar, df[, x], "lrm",
-                      c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar)))
-cv.ols <- lapply(ols.vars, function(x) CallCV(specs, as.integer(df[, x]), "ols",
-                 c("log GDP per cap. + log Pop.", ivar.labels)))
-cv.ols.cwar <- lapply(ols.vars, function(x) CallCV(specs.cwar, as.integer(df[, x]), "ols",
-                      c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar)))
+cv.lrm <- lapply(lrm.vars, function(x)
+                 lapply(seq(1, MI_ITER), function(z)
+                 CallCV(df.mi[[z]], specs, df.mi[[z]][, x],
+                 "lrm", c("log GDP per cap. + log Pop.", ivar.labels))))
+cv.lrm.cwar <- lapply(lrm.vars, function(x)
+                      lapply(seq(1, MI_ITER), function(z)
+                      CallCV(df.mi[[z]], specs.cwar, df.mi[[z]][, x], "lrm",
+                      c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar))))
+cv.ols <- lapply(ols.vars, function(x)
+                 lapply(seq(1, MI_ITER), function(z)
+                 CallCV(df.mi[[z]], specs, as.integer(df.mi[[z]][, x]), "ols",
+                 c("log GDP per cap. + log Pop.", ivar.labels))))
+cv.ols.cwar <- lapply(ols.vars, function(x)
+                      lapply(seq(1, MI_ITER), function(z)
+                      CallCV(df.mi[[z]], specs.cwar, as.integer(df.mi[[z]][, x]), "ols",
+                      c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar))))
 cv <- c(cv.lrm, cv.lrm.cwar, cv.ols, cv.ols.cwar)
+cv <- lapply(cv, function(x) do.call(rbind, x))
+cv <- lapply(cv, function(x) ddply(x[, -4], .(x$spec), colMeans))
