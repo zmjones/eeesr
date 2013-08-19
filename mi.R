6227be6c11a40dad0e0ef8f5186446cdef308734
Zachary M. Jones <zmj@zmjones.com>
2013-08-19 16:40:16 -0400

    fixed method error

--- mi.R
+++ mi.R
@@ -2,7 +2,7 @@ invisible(require("mice"))
 
 methods <- c(rep("", 3), rep("ri", 5), rep("", 5), "", "", rep("quadratic", 3),
              "", "ri", "quadratic", "quadratic", rep("cart", 7), "", "", "cart",
-             "", "", "ri", "cart", "", "", rep("cart", 3))
+             "", "", "ri", "cart", "", rep("cart", 4))
 
 mi <- mice(df.imp, m = MI_ITER, method = methods, print = FALSE)
 df.mi <- lapply(seq(1, MI_ITER), function(x) complete(mi, x))
