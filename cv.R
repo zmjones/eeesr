8fd4bddb179b91d5259478aed872803685ffae42
Zachary M. Jones <zmj@zmjones.com>
2013-08-30 15:48:12 -0400

    makes cv function calls more terse
    wraps call to CallCV in CleanCV, rather than doing the same thing on two lines

--- cv.R
+++ cv.R
@@ -1,12 +1,12 @@
 invisible(lapply(c("rms", "multicore", "plyr"), require, character.only = TRUE))
 options(showprogress = FALSE)
 
-cv.lrm <- CallCV(lrm.vars, specs, "lrm")
-cv.lrm <- CleanCV(cv.lrm, c("log GDP per cap. + log Pop.", ivar.labels))
-cv.lrm.lcwar <- CallCV(lrm.vars, specs.cwar, "lrm")
-cv.lrm.cwar <- CleanCV(cv.lrm, c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar))
-cv.ols <- CallCV(ols.vars, specs, "ols")
-cv.ols <- CleanCV(cv.ols, c("log GDP per cap. + log Pop.", ivar.labels))
-cv.ols.cwar <- CallCV(ols.vars, specs.cwar, "ols")
-cv.ols.cwar <- CleanCV(cv.ols, c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar))
+cv.lrm <- CleanCV(CallCV(lrm.vars, specs, "lrm"),
+                  c("log GDP per cap. + log Pop.", ivar.labels))
+cv.lrm.cwar <- CleanCV(CallCV(lrm.vars, specs.cwar, "lrm"),
+                       c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar))
+cv.ols <- CleanCV(CallCV(ols.vars, specs, "ols"),
+                  c("log GDP per cap. + log Pop.", ivar.labels))
+cv.ols.cwar <- CleanCV(CallCV(ols.vars, specs.cwar, "ols"),
+                       c("log GDP per cap. + log Pop. + Civil War", ivar.labels.cwar))
 cv <- c(cv.lrm, cv.lrm.cwar, cv.ols, cv.ols.cwar)
