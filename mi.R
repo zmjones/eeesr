539c1e6d439f2854fd211581fe8ad32230b42b87
Zachary M. Jones <zmj@zmjones.com>
2014-04-25 13:54:49 -0400

    changed mi methods

--- mi.R
+++ mi.R
@@ -1,8 +1,8 @@
 require(mice)
 
 methods <- c(rep("", 3), rep("ri", 5), rep("", 5), "", "", rep("ri", 3),
-             "", rep("ri", 3), rep("cart", 7), "", "", "cart",
-             "", "", "ri", "cart", rep("", 2), rep("cart", 4), rep("", 7))
+             "rf", rep("ri", 3), rep("rf", 7), "", "", "rf",
+             "", "", "ri", "rf", rep("", 2), rep("rf", 4), rep("", 7))
 
 mi <- mice(df, m = MI_ITER, method = methods, print = FALSE)
 df.mi <- lapply(seq(1, MI_ITER), function(x) complete(mi, x))
