e5ee50b65a489e9dcac6a28ca34e34e6734d5e5e
Zachary M. Jones <zmj@zmjones.com>
2014-04-30 16:38:33 -0400

    move variable transformations to setup

--- data.R
+++ data.R
@@ -134,11 +134,8 @@ df$int[is.na(df$int)] <- 0
 df$cumint[is.na(df$cumint)] <- 0
 df$military[is.na(df$military)] <- 0
 df$military[df$military == -999] <- NA
-df$ingo_uia <- log(df$ingo_uia + 1)
 df$cwar <- ifelse((df$type == 3 | df$type == 4) & df$cumint == 1, 1, 0)
 df$iwar <- ifelse(df$type == 2 & df$cumint == 1, 1, 0)
-df$rentspc <- log(df$rentspc + 1)
-df$trade_gdp <- log(df$trade_gdp)
 df$execrlc[df$execrlc == -999] <- NA
 
 df <- aggregate(df, by = list(df$ccode, df$year), max)[, -c(1:2)]
