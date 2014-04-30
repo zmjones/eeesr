9484938a2be12a8fef2e58e1d0131369f6a1bda5
Zachary M. Jones <zmj@zmjones.com>
2014-04-30 11:49:13 -0400

    adds analysis of iwar=1 observations on betas in lrm

---
+++ leverage.R
@@ -0,0 +1,18 @@
+require(rms)
+source("setup.R")
+
+check <- function(form) {
+    fit <- lrm(as.formula(form), df, y = TRUE, x = TRUE)
+    suppressWarnings(infl <- residuals(fit, type = "dfbetas"))
+    test <- data.frame("iwar" = fit$x[, 3], "infl" = na.omit(infl[, 4]))
+    out <- test[test$iwar == 1, ]
+    print(form)
+    data.frame(df[row.names(out), c(1,3)], "dfbeta" = out$infl)
+}
+
+check("physint ~ gdppc + pop + iwar")
+check("amnesty ~ gdppc + pop + iwar")
+check("disap ~ gdppc + pop + iwar")
+check("polpris ~ gdppc + pop + iwar")
+check("tort ~ gdppc + pop + iwar")
+check("kill ~ gdppc + pop + iwar")
